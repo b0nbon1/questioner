8d4e2d3f2022c965c844b9d17f063a3a541936e4
b0nbon1 <b0nbon@yahoo.com>
2019-01-08 08:22:09 +0300

    [finishes #163043188]adds  test factory tests

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from instance.config import app_config
+from flask import json
 
 
 def create_app(config_name):
 
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object(app_config[config_name])
+    app.config.from_object(app_config)
     app.config.from_pyfile('config.py')
 
     # factory set app
     @app.route('/hello')
     def hello():
-        return jsonify({"message": "hello, world"})
+        return make_response(jsonify({"message": "hello, world!"})), 200
 
     return app
