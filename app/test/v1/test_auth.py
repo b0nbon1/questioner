8bd2c27a87a61a1ec22ca40d50f982547698dfc6
b0nbon1 <b0nbon@yahoo.com>
2019-01-08 11:27:23 +0300

    [finishes #163042640] adds endpoint api/v1/auth/login

--- app/test/v1/test_auth.py
+++ app/test/v1/test_auth.py
@@ -6,6 +6,13 @@ class AuthActions(object):
     def __init__(self, client):
         self._client = client
 
+    def login(self, username='pytest2', password='test2guy'):
+        return self._client.post(
+            '/api/v1/auth/login',
+            data=json.dumps({'username': username, 'password': password}),
+            content_type='application/json'
+        )
+
     def register(self, firstname='test1',
                  lastname='test2',
                  othername='test3',
@@ -33,7 +40,22 @@ def auth(client):
     return AuthActions(client)
 
 
-def test_register(client, auth):
-    response = auth.register()
+def test_login(client, auth):
+    response = auth.login()
+
     assert response.status_code == 200
 
+
+@pytest.mark.parametrize(('username', 'password', 'message'), (
+    ('not', 'test', b'user not found'),
+    ('pytest2', 'guess', b'wrong password'),
+    ('pytest2', 'test2guy', b'Successfully Logged In')
+))
+def test_login_validate_input(auth, username, password, message):
+    response = auth.login(username, password)
+    assert message in response.data
+
+
+def test_register(client, auth):
+    response = auth.register()
+    assert response.status_code == 201
