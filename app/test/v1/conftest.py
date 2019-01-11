a36542a9c866d611790e3685df882020f81b1434
b0nbon1 <b0nbon@yahoo.com>
2019-01-11 08:42:57 +0300

    [Chore #163139745] adds jwt to the tests

--- app/test/v1/conftest.py
+++ app/test/v1/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from app import create_app
+from flask_jwt_extended import create_access_token
 
 
 @pytest.fixture
--- app/test/v1/test_questions.py
+++ app/test/v1/test_questions.py
@@ -4,8 +4,9 @@ from flask import json
 
 class Setup_question():
     # setups the tests
-    def __init__(self, client):
+    def __init__(self, client, headers):
         self._client = client
+        self._headers = headers
 
     def Create_question(self, user=2,
                         meetup=1,
@@ -19,47 +20,47 @@ class Setup_question():
                              'title': title,
                              'body': body,
                              }),
-            content_type='application/json'
+            content_type='application/json', headers=self._headers
         )
 
     def vote(self, url):
         return self._client.patch(
             url,
-            content_type='application/json'
+            content_type='application/json', headers=self._headers
         )
 
 
 # creates a fixture for class questions
 @pytest.fixture
-def questions(client):
-    return Setup_question(client)
+def questions(client, headers):
+    return Setup_question(client, headers)
 
 
-def test_create_question(client, questions):
+def test_create_question(questions):
     response = questions.Create_question()
 
     assert response.status_code == 201
 
 
-def test_voteup(client, questions):
+def test_voteup(questions):
     response = questions.vote('/api/v1/question/upvote/1')
 
     assert response.status_code == 200
 
 
-def test_votedown(client, questions):
+def test_votedown(questions):
     response = questions.vote('/api/v1/question/downvote/1')
 
     assert response.status_code == 200
 
 
-def test_fail_votedown(client, questions):
+def test_fail_votedown(questions):
     response = questions.vote('/api/v1/question/downvote/5')
 
-    assert response.status_code == 500
+    assert response.status_code == 404
 
 
-def test_fail_voteup(client, questions):
+def test_fail_voteup(questions):
     response = questions.vote('/api/v1/question/upvote/5')
 
-    assert response.status_code == 500
+    assert response.status_code == 404
