8ac2e6b9285458244216b109626b70eb076325c0
b0nbon1 <b0nbon@yahoo.com>
2019-01-15 20:19:09 +0300

    [chore #163214900]adds regester voter

--- app/api/v1/views/questions.py
+++ app/api/v1/views/questions.py
@@ -1,6 +1,7 @@
 from app.api.v1.models.model_questions import Questions, questions
 from flask import Flask, jsonify, make_response, request, Blueprint
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from ..models.model_users import Users
 
 
 question = Blueprint('questions', __name__, url_prefix='/api/v1/question')
@@ -24,22 +25,19 @@ def ask_question():
 @question.route('/upvote/<int:question_id>', methods=['PATCH'])
 @jwt_required
 def vote_up(question_id):
-    try:
-        meetup = [question for question in questions if
-                  question['id'] == question_id][0]['meetup']
-        title = [question for question in questions if
-                 question['id'] == question_id][0]['title']
-        body = [question for question in questions if
-                question['id'] == question_id][0]['body']
-    except IndexError:
-        return make_response(jsonify(
-            {
-                "status": 404,
-                "error": "error can't find meetup data"
-            })), 404
+    question = [
+            question for question in questions if question['id'] == question_id]
+    if len(question) == 0:
+            return make_response(jsonify({"error": "no available questions right now",
+                                      "status": 404})), 404
+    user = get_jwt_identity()
+    user_id = [u for u in Users if u['public_id'] == user][0]['id']
 
-    vote = Questions().add_upvote(meetup, title, body, question_id)
-
+    votes = Questions().add_vote(question_id, user_id)
+    if votes is False:
+        return make_response(jsonify({"error": "you can't vote",
+                                      "status": 400})), 400
+    vote = votes[0]
     vote['upvotes'] = vote['upvotes'] + 1
 
     return make_response(jsonify({'status': 200,
@@ -49,23 +47,18 @@ def vote_up(question_id):
 @question.route('/downvote/<int:question_id>', methods=['PATCH'])
 @jwt_required
 def vote_down(question_id):
-    try:
-        meetup = [question for question in questions if
-                  question['id'] == question_id][0]['meetup']
-        title = [question for question in questions if
-                 question['id'] == question_id][0]['title']
-        body = [question for question in questions if
-                question['id'] == question_id][0]['body']
-
-    except IndexError:
-        return make_response(jsonify(
-            {
-                "status": 404,
-                "error": "error can't find meetup data"
-            })), 404
-
-    vote = Questions().add_downvote(meetup, title, body, question_id)
-
+    question = [
+            question for question in questions if question['id'] == question_id]
+    if len(question) == 0:
+            return make_response(jsonify({"error": "no available questions right now",
+                                      "status": 404})), 404
+    user = get_jwt_identity()
+    user_id = [u for u in Users if u['public_id'] == user][0]['id']
+    votes = Questions().add_vote(question_id, user_id)
+    if votes is False:
+        return make_response(jsonify({"message": "you have already voted",
+                                      "status": 400})), 400
+    vote = votes[0]
     vote['downvotes'] = vote['downvotes'] + 1
 
     return make_response(jsonify({'status': 200,
